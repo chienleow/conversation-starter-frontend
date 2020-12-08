6086f831b4473d1cb0e6f591ca7758af22ebcb27
chienleow <chienleow1@gmail.com>
2020-12-08 00:27:53 -0500

    create variables to capture form input

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const endPoint = "http://localhost:3000/api/v1/questions"
 const openQuestion = document.querySelector("#open-question");
-let questionContainer = document.getElementById("open-question").innerHTML
+let questionContainer = document.getElementById("open-question").innerHTML // should I change this back to const?
+const createForm = document.querySelector("#create-form")
 
 document.addEventListener('DOMContentLoaded', () => {
     openQuestion.addEventListener("click", displayQuestion);
+    createForm.addEventListener("submit", (e) => postForm(e));
 })
 
 function displayQuestion() {
@@ -29,4 +31,16 @@ function getQuestion() {
 
 function clearContainer() {
     questionContainer = "";
+}
+
+function postForm(e) {
+    e.preventDefault()
+    const usernameInput = document.querySelector("#username").value
+    const questionInputOne = document.querySelector("#question1").value
+    postFetch(usernameInput, questionInputOne)
+    // question: can I select all my questions at once?
+}
+
+function postFetch(username, question_one) {
+    console.log(username, question_one);
 }
